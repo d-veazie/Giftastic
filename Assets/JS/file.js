54b2b67c1ad2c868679d29fe212eaa0732b9ba72
d-veazie <dmanvz@gmail.com>
2019-06-20 15:07:09 -0600

    updated code

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -1,6 +1,30 @@
+
+
+let startGifs = ["trippy", "funny", "happy", "crazy"]; 
+
+function makeButtons(){ 
+	$("#buttons-view").empty();
+	for (let i = 0; i < startGifs.length; i++){
+		let a = $("<button>") 
+		a.addClass("startGifs"); 
+		a.attr("data-name", startGifs[i]); 
+		a.text(startGifs[i]); 
+		$("#buttons-view").append(a); 
+	}
+}
+
+$("#addGif").on("click", function(event){
+    event.preventDeafault(); 
+	
+	let startGifs = $("#startGifs-input").val().trim();
+	startGifs.push(startGifs);
+	makeButtons();
+	return false; 
+})
+
 function displayGifInfo() {
     let gif = $(this).attr("data-name");
-    let URL = "https://api.giphy.com/v1/gifs/" + gif + "&api_key=uXQAe2arj6gUtZ2wDLXzZ2hCJKlI3Vng";
+    let URL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=uXQAe2arj6gUtZ2wDLXzZ2hCJKlI3Vng";
 
     $.ajax({
         url: URL,
@@ -10,11 +34,24 @@ function displayGifInfo() {
         console.log(URL);
         console.log(response); 
 
-        $("buttonLocation").text(JSON.stringify(response));
+        $("#buttons-view").text(JSON.stringify(response));
 
     });
 
+}
+
+function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (let i = 0; i < gif.length; i++) {
+        let a = $("<button>");
+        a.attr("data-name", gif[i]);
+        a.text(gif[i]);
+        $("#buttons-view").append(a); 
 
 
+    }
 
 }
+
+makeButtons(); 
